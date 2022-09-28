3816f29c3ebf360969cbe1bf546132c0d214d895
Thomas <ayanto.resume@gmail.com>
2022-09-28 17:03:48 +0200

    add who component

---
+++ src/app/who-iam/who-iam.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-who-iam',
+  templateUrl: './who-iam.component.html',
+  styleUrls: ['./who-iam.component.css']
+})
+export class WhoIamComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
